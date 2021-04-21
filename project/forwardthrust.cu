62c2dc287f889e2f7c68cdbc2d6facdb4fa78c3a
Navya Chandra <navyac@ghc79.ghc.andrew.cmu.edu>
2021-04-20 22:56:48 -0400

    project start

---
+++ project/forwardthrust.cu
@@ -0,0 +1,39 @@
+// System includes
+#include <stdio.h>
+#include <assert.h>
+
+// CUDA runtime
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <driver_functions.h>
+#include "CycleTimer.h"
+
+#include <thrust/sort.h>
+#include <thrust/reduce.h>
+#include <thrust/device_ptr.h>
+#include <thrust/functional.h>
+#include "lib.h"
+
+
+
+
+void
+printCudaInfo() {
+    int deviceCount = 0;
+    cudaError_t err = cudaGetDeviceCount(&deviceCount);
+
+    printf("---------------------------------------------------------\n");
+    printf("Found %d CUDA devices\n", deviceCount);
+
+    for (int i=0; i<deviceCount; i++) {
+        cudaDeviceProp deviceProps;
+        cudaGetDeviceProperties(&deviceProps, i);
+        printf("Device %d: %s\n", i, deviceProps.name);
+        printf("   SMs:        %d\n", deviceProps.multiProcessorCount);
+        printf("   Global mem: %.0f MB\n",
+               static_cast<float>(deviceProps.totalGlobalMem) / (1024 * 1024));
+        printf("   CUDA Cap:   %d.%d\n", deviceProps.major, deviceProps.minor);
+        printf("   Shared memory per block:   %d bytes\n", deviceProps.sharedMemPerBlock);
+    }
+    printf("---------------------------------------------------------\n");
+}
